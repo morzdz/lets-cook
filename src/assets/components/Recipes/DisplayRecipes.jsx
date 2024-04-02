4e8fcd869058253481ad8249e70987a0be55c167
morzdz <manon.drozdz@gmail.com>
2024-04-02 16:31:46 +0200

    'DisplayRecipes' (qui permet de gérer l'affichage des reccettes) fini

--- src/assets/components/Recipes/DisplayRecipes.jsx
+++ src/assets/components/Recipes/DisplayRecipes.jsx
@@ -31,30 +31,56 @@ const DisplayRecipes = () => {
     return (
         <>
             <div className="bg-white py-24 sm:py-32">
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="mx-auto max-w-2xl lg:mx-0">
-                    <h2 className="text-3xl font-bold tracking-tight text-theme1-main sm:text-4xl">Let's Cook</h2>
-                    <SearchBar handleChange={handleChange} handleSearch={handleSearch} searchTerm={searchTerm}/>
-                </div>
-                <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                {data.map((post) => (
-                    <Card 
-                        key={post.id} 
-                        id={post.id} 
-                        image={post.imageUrl}
-                        title={post.title}
-                        author={post.author}
-                        date={post.date}
-                        difficulty={post.difficulty}
-                        category={post.category}
-                        description={post.description}
-                    />
-                ))}
+                <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div className="mx-auto max-w-2xl lg:mx-0">
+                        <h2 className="text-3xl font-bold tracking-tight text-theme1-main sm:text-4xl">Let's Cook</h2>
+                        <SearchBar handleChange={handleChange} handleSearch={handleSearch} searchTerm={searchTerm}/>
+                    </div>
+                    <div>
+                        {searchTerm === "" ? (
+                            <h2 className="text-3xl tracking-tight">Toutes les recettes</h2>
+                        ) : (
+                            <h2 className="text-3xl tracking-tight">Search results for : {searchTerm}</h2>
+                        )}
+                    </div>
+                    <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+                        {filteredRecipe.length > 0 ? (
+                            // Affichage des recettes filtrées
+                            filteredRecipe.map((post) => (
+                                <Card 
+                                    key={post.id} 
+                                    id={post.id} 
+                                    image={post.imageUrl}
+                                    title={post.title}
+                                    author={post.author}
+                                    date={post.date}
+                                    difficulty={post.difficulty}
+                                    category={post.category}
+                                    description={post.description}
+                                />
+                            ))
+                        ) : (
+                            // Affichage de toutes les recettes
+                            data.map((post) => (
+                                <Card 
+                                    key={post.id} 
+                                    id={post.id} 
+                                    image={post.imageUrl}
+                                    title={post.title}
+                                    author={post.author}
+                                    date={post.date}
+                                    difficulty={post.difficulty}
+                                    category={post.category}
+                                    description={post.description}
+                                />
+                            ))
+                        )}
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
+    
 }
 
 export default DisplayRecipes
