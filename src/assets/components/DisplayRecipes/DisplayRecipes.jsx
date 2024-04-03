a733db40f5e40fbb7ead74233f2c6326766410f4
morzdz <manon.drozdz@gmail.com>
2024-04-03 11:55:05 +0200

    fix du de l'affichage suite au clic du bouton reset

--- src/assets/components/DisplayRecipes/DisplayRecipes.jsx
+++ src/assets/components/DisplayRecipes/DisplayRecipes.jsx
@@ -53,7 +53,7 @@ const DisplayRecipes = () => {
                         )}
                     </div>
                     <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                        {filteredRecipe.length > 0 ? (
+                        {filteredRecipe.length > 0 && searchTerm.length > 0 ? (
                             // Affichage des recettes filtrées
                             filteredRecipe.map((post) => (
                                 <Card 
