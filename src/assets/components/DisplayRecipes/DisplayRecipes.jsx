a4c7fe44b70f9840f0b54279d80e11c4ed85799d
morzdz <manon.drozdz@gmail.com>
2024-04-22 15:50:14 +0200

    modif du composant favoris afin de rendre le code plus lisible, ce composant n'est pas encore fonctionnel => l'affichage n'est pas correcte

--- src/assets/components/DisplayRecipes/DisplayRecipes.jsx
+++ src/assets/components/DisplayRecipes/DisplayRecipes.jsx
@@ -4,7 +4,6 @@ import SearchBar from '../SearchBar/SearchBar';
 import FavButton from '../FavButton/FavButton';
 import Reset from '../Reset/Reset';
 import data from '../../data/recettes.json';
-import FavoriteRecipes from '../FavoritesRecipes/FavoritesRecipes';
 import 'tailwindcss/tailwind.css';
 
 const DisplayRecipes = () => {
@@ -12,8 +11,6 @@ const DisplayRecipes = () => {
     const [filteredRecipe, setFilteredRecipe] = useState([]);
     const [showFavorites, setShowFavorites] = useState(false);
     const [favorites, setFavorites] = useState([]); // État pour les recettes favorites
-
-
 
     const handleSearch = (term) => {
         // Vérification si le terme de recherche est vide
@@ -65,47 +62,65 @@ const DisplayRecipes = () => {
                         )}
                     </div>
                     <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                        {showFavorites ? (
-                            // Affichage des recettes favorites
-                            <FavoriteRecipes favorites={favorites} addToFav={addToFav} />
+                    {showFavorites ? (
+                        // Affichage des recettes favorites
+                        favorites.length === 0 ? (
+                            <h3>No recipes found</h3>
                         ) : (
-                            // Affichage des recettes filtrées ou toutes les recettes normales
-                            filteredRecipe.length > 0 && searchTerm.length > 0 ? (
-                                // Affichage des recettes filtrées
-                                filteredRecipe.map((post) => (
-                                    <Card 
-                                        key={post.id} 
-                                        id={post.id} 
-                                        image={post.imageUrl}
-                                        title={post.title}
-                                        author={post.author}
-                                        date={post.date}
-                                        difficulty={post.difficulty}
-                                        category={post.category}
-                                        description={post.description}
-                                        addToFav={addToFav} // Passer la fonction addToFav comme prop
-                                        favorites={favorites} // Passer les favoris comme prop
-                                    />
-                                ))
-                            ) : (
-                                // Affichage de toutes les recettes
-                                data.map((post) => (
-                                    <Card 
-                                        key={post.id} 
-                                        id={post.id} 
-                                        image={post.imageUrl}
-                                        title={post.title}
-                                        author={post.author}
-                                        date={post.date}
-                                        difficulty={post.difficulty}
-                                        category={post.category}
-                                        description={post.description}
-                                        addToFav={addToFav} // Passer la fonction addToFav comme prop
-                                        favorites={favorites} // Passer les favoris comme prop
-                                    />
-                                ))
-                            )
-                        )}
+                            favorites.map((post) => (
+                                <Card 
+                                    key={post.id} 
+                                    id={post.id} 
+                                    image={post.imageUrl}
+                                    title={post.title}
+                                    author={post.author}
+                                    date={post.date}
+                                    difficulty={post.difficulty}
+                                    category={post.category}
+                                    description={post.description}
+                                    addToFav={addToFav} // Passer la fonction addToFav comme prop
+                                    favorites={favorites} // Passer les favoris comme prop
+                                />
+                            ))
+                        )
+                    ) : (
+                        // Affichage des recettes filtrées ou toutes les recettes normales
+                        filteredRecipe.length > 0 && searchTerm.length > 0 ? (
+                            // Affichage des recettes filtrées
+                            filteredRecipe.map((post) => (
+                                <Card 
+                                    key={post.id} 
+                                    id={post.id} 
+                                    image={post.imageUrl}
+                                    title={post.title}
+                                    author={post.author}
+                                    date={post.date}
+                                    difficulty={post.difficulty}
+                                    category={post.category}
+                                    description={post.description}
+                                    addToFav={addToFav} // Passer la fonction addToFav comme prop
+                                    favorites={favorites} // Passer les favoris comme prop
+                                />
+                            ))
+                        ) : (
+                            // Affichage de toutes les recettes
+                            data.map((post) => (
+                                <Card 
+                                    key={post.id} 
+                                    id={post.id} 
+                                    image={post.imageUrl}
+                                    title={post.title}
+                                    author={post.author}
+                                    date={post.date}
+                                    difficulty={post.difficulty}
+                                    category={post.category}
+                                    description={post.description}
+                                    addToFav={addToFav} // Passer la fonction addToFav comme prop
+                                    favorites={favorites} // Passer les favoris comme prop
+                                />
+                            ))
+                        )
+                    )}
                     </div>
                 </div>
             </div>
